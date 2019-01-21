141eaa9dcb3f0191033b6e8e56f655a4712cc0cc
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-01-21 15:55:29 +0800

    introduced some error

--- src/test/java/OtherTest.java
+++ src/test/java/OtherTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OtherTest {
 
     @Test
-    public void test() {assertEquals(1,1);}
+    public void test() {assertEquals(3,1);}
 }
 
 class YetAnotherTest {
 
     @Test
-    public void test() {assertEquals(3,3);}
+    public void test() {assertEquals(3,1);}
 }
